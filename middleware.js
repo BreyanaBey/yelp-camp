23ae09c27361362a3e866b2e1309039981ffcee1
breyanabey <bpb5485@psu.edu>
2021-07-20 12:35:39 -0400

    initial commit

--- middleware.js
+++ middleware.js
@@ -17,6 +17,7 @@ module.exports.isLoggedIn = (req, res, next) => {
 
 // Check for errors
 module.exports.validateCampground = (req, res, next) => {
+    //
     const { error } = campgroundSchema.validate(req.body)
     if(error) {
         const msg = error.details.map(el => el.message).join(',')
